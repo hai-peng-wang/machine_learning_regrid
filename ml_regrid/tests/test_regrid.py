6c547d1d6e2113c48aecd49dc38b1582d19c995f
hai-peng-wang <haipeng.wang@bom.gov.au>
2017-10-06 15:44:16 +1100

    Update test_regrid.py

--- ml_regrid/tests/test_regrid.py
+++ ml_regrid/tests/test_regrid.py
@@ -10,124 +10,16 @@ from ml_regrid.iris_tools import (
     create_derive_cube, get_cube_grid_points, get_land_sea_index,
     transform_cube_by_masked_index, update_cube_with_new_data)
 
+from ml_regird.tests.prepare_sample_for_test import (
+    dpt_scn_aps3, lsm_aps2, lsm_aps3, precip_aps3,
+    q_scn_aps3, sfc_prs_aps3, t_scn_aps3,
+    topo_aps2, topo_aps3, u10_aps3, v10_aps3)
+
 import numpy as npfrom numpy.testing import (
     assert_almost_equal, assert_array_equal, assert_raises)
 
 from scipy.interpolate import RegularGridInterpolator
 
-lsm_tgt_data = np.array(
-        [[1., 0., 0.],
-         [1., 1., 0.],
-         [1., 1., 0.]])
-
-surface_alt_tgt_data = np.array(
-        [[40., 0., 0.],
-         [272.5, 49.5, 0.],
-         [569., 139.375, 0.]])
-
-# For src, the index points lsm_src.data[821:827, 461:467], shape=(6, 6)
-lsm_src_data = np.array(
-      [[1, 1, 0, 0, 0, 0],
-       [1, 1, 1, 0, 0, 0],
-       [1, 1, 1, 1, 0, 0],
-       [1, 1, 1, 1, 0, 0],
-       [1, 1, 1, 1, 0, 0],
-       [1, 1, 1, 1, 0, 0]])
-
-surface_alt_src_data = np.array(
-        [[30.79901695, 24.64077759, 0., 0., 0., 0.],
-         [40.51713181, 57.62937927, 44.1620636, 0., 0., 0.],
-         [61.04179001, 77.5438385, 68.0868454, 27.50967216, 0., 0.],
-         [197.31484985, 109.88461304, 81.08721161, 47.29803085, 0., 0.],
-         [500.52633667, 204.61886597, 96.57264709, 57.00541687, 0., 0.],
-         [801.8571167, 337.81097412, 131.99372864, 66.28527832, 0., 0.]])
-
-t_scn_src_data = np.array(
-     [[300.0625, 299.1875, 300.859375, 300.4375, 300.640625, 300.8125],
-      [300.734375, 303.546875, 300.828125, 300.53125, 300.578125, 300.796875],
-      [302.0625, 301.078125, 300.703125, 299.90625, 300.640625, 300.71875],
-      [300.796875, 299.890625, 299.703125, 300.046875, 300.71875, 300.65625],
-      [297.078125, 298.875, 299.09375, 300.078125, 300.71875, 300.65625],
-      [296.125, 297.875, 298.5625, 299.328125, 300.59375, 300.734375]])
-
-sfc_prs_src_data = np.array(
-      [[1212528., 1212361., 1212363., 1212647., 1212919., 1213073.],
-       [1212195., 1211922., 1212181., 1212605., 1212950., 1213131.],
-       [1212043., 1212038., 1212174., 1212588., 1212954., 1213181.],
-       [1212325., 1212124., 1212306., 1212599., 1212949., 1213226.],
-       [1212454., 1212399., 1212434., 1212652., 1213012., 1213305.],
-       [1212653., 1212496., 1212555., 1212774., 1213106., 1213395.]])
-
-dpt_scn_src_data = np.array(
-    [[293.515625, 294.03125, 294.5625, 294.53125, 294.171875, 294.],
-     [293.1875, 290.59375, 292.15625, 294.078125, 294.09375, 293.6875],
-     [292.59375, 291.71875, 291.390625, 291.890625, 293.828125, 293.640625],
-     [291.25, 291.8125, 291.296875, 291.328125, 293.65625, 293.515625],
-     [292.03125, 291.734375, 291.515625, 291.015625, 293.296875, 293.359375],
-     [292.328125, 291.4375, 291.875, 291.3125, 292.90625, 293.171875]])
-
-
-def empty_3d_cube_src(data, name=None, unit=None, stash=None, **kwargs):
-    """
-    Prepare some iris cubes at src grids for testing
-    """
-    if data is None:
-        data = np.empty([6, 6])
-
-    cube = Cube(data)
-
-    time = AuxCoord([0], 'time', units='hours since epoch')
-
-    latitude = DimCoord([6.26953125, 6.38671875, 6.50390625,
-                         6.62109375, 6.73828125, 6.85546875],
-                       	standard_name='latitude', units='degrees')
-
-    longitude = DimCoord([81.12304688, 81.29882812, 81.47460938,
-                          81.65039062, 81.82617188, 82.00195312],
-                         standard_name='longitude', units='degrees')
-    cube.add_dim_coord(latitude, 0)
-    cube.add_dim_coord(longitude, 1)
-    cube.add_aux_coord(time)
-
-    if name:
-        cube.long_name = name
-    if unit:
-        cube.units = unit
-    if stash:
-        cube.attributes['STASH'] = stash
-
-    return cube
-
-def empty_3d_cube_src(data, name=None, unit=None, stash=None, **kwargs):
-    """
-    Prepare some iris cubes at src grids for testing
-    """
-    if data is None:
-        data = np.empty([6, 6])
-
-    cube = Cube(data)
-
-    time = AuxCoord([0], 'time', units='hours since epoch')
-
-    latitude = DimCoord([6.26953125, 6.38671875, 6.50390625,
-                         6.62109375, 6.73828125, 6.85546875],
-                       	standard_name='latitude', units='degrees')
-
-    longitude = DimCoord([81.12304688, 81.29882812, 81.47460938,
-                          81.65039062, 81.82617188, 82.00195312],
-                         standard_name='longitude', units='degrees')
-    cube.add_dim_coord(latitude, 0)
-    cube.add_dim_coord(longitude, 1)
-    cube.add_aux_coord(time)
-
-    if name:
-        cube.long_name = name
-    if unit:
-        cube.units = unit
-    if stash:
-        cube.attributes['STASH'] = stash
-
-    return cube
 
 def regrid_cube_by_scheme(param_cube, target_cube, scheme=None):
     """
@@ -168,20 +60,20 @@ def regrid_cube_by_scheme(param_cube, target_cube, scheme=None):
 class TestMdsRegrid(unittest.TestCase):
     def setUp(self):
         # Create sample cubes for testing
-        self.topo_tgt = empty_3d_cube_tgt(
-            surface_alt_tgt_data, 'surface_altitude', 'm', 'm01s00i033')
-        self.topo_src = empty_3d_cube_src(
-            surface_alt_src_data, 'surface_altitude', 'm', 'm01s00i033')
-        self.lsm_tgt = empty_3d_cube_tgt(
-            lsm_tgt_data, 'land_area_fraction', '1', )
-        self.lsm_src = empty_3d_cube_src(
-            lsm_src_data, 'land_binary_mask', '1')
-        self.t_scn_src = empty_3d_cube_src(
-            t_scn_src_data, 'air_temperature', 'K')
-        self.sfc_prs_src = empty_3d_cube_src(
-            sfc_prs_src_data, 'air_pressure_at_sea_level', 'Pa')
-        self.dpt_scn_src = empty_3d_cube_src(
-            dpt_scn_src_data, 'dew_point_temperature', 'K', 'm01s03i250')
+        self.topo_aps2 = topo_aps2
+        self.topo_aps3 = topo_aps3
+        self.lsm_aps2 = lsm_aps2
+        self.lsm_aps3 = lsm_aps3
+        self.t_scn_aps3 = t_scn_aps3
+        self.sfc_prs_aps3 = sfc_prs_aps3
+        self.precip_aps3 = precip_aps3
+        self.dpt_scn_aps3 = dpt_scn_aps3
+        self.q_scn_aps3 = q_scn_aps3
+
+        # Note that u10 is sitting on different longitude (x)
+        self.u10_aps3 = u10_aps3
+
+        self.v10_aps3 = v10_aps3
 
         # For (x, y), it should be (lon, lat) accordingly
         self.x_tgt = self.topo_tgt.coord('longitude').points
@@ -229,3 +121,94 @@ class TestMdsRegrid(unittest.TestCase):
         scipy_results = drv_t_scn_scipy.data
         iris_results = self.drv_t_scn_iris.data
         assert_almost_equal(iris_results, scipy_results, decimal=2)
+
+    def test_t_scn_by_regrid_linear_iris_coastline_correction(self):
+        """
+        Testing the regrid_iris_coastline_correction
+        """
+        # Need to find the coastline points first
+        # xv, yv = np.meshgrid(self.x_aps2, self.y_aps2)
+        # tgrid = np.dstack((xv, yv))
+
+        # weight = self.interpolator.compute_interp_weights(tgrid)
+        # print(weight)
+        # output_data = interpolator.interp_using_pre_computed_weights(weights)
+        # Then add land/sea mask for coastline correction
+
+        regridder = MdsRegridder(self.topo_aps3, self.topo_aps2,
+                                 self.lsm_aps3, self.lsm_aps2)
+        t_scn_by_iris_cc_list = \
+            regridder._regrid_iris_coastline_correction(self.t_scn_aps3)
+        [t_scn_by_iris_cc] = t_scn_by_iris_cc_list.extract('air_temperature')
+        # Should be different with/without coastline correction
+        # For the sample, only values at two mixed pnt are changed
+        # Here 'Expected value' are values after the coastline correstion
+        # Index  -/x   Delta     Target   Expected value   Actual Value
+        # 1     x   0.031209  0.002000    300.695034      300.663825
+        # 11     x   1.302968  0.002000    300.765625      299.462657
+        self.show_difference(self.drv_t_scn_iris, t_scn_by_iris_cc,
+                             entropy=1.334, accuracy=1.31)
+
+    def test_t_scn_by_regrid_nearest_iris_coastline(self):
+        """
+        Test nearest regrid with coastline correction
+        :return: Just show the difference between linear and nearest
+        """
+        # Get GFE results
+        # drv_gfe_coastline_nearest = regrid_nearest_gfe_coastline_pigps(
+        #    self.input_cubes, self.in_grids, self.out_grids)
+        # drv_t_scn_gfe_coastline = extract_cube_by_name(
+        #   drv_gfe_coastline_nearest, 'air_temperature')
+        # Note: GFE results have 'nan' for the third column
+
+        # Get Iris results
+        drv_iris_coastline_nearest = regrid_nearest_iris_coastline(
+            self.input_cubes, self.in_grids, self.out_grids)
+        drv_t_scn_iris_coastline = extract_cube_by_name(
+            drv_iris_coastline_nearest, 'air_temperature')
+
+        self.show_difference(self.drv_t_scn_iris, drv_t_scn_iris_coastline,
+                             entropy=7.050, accuracy=1.7)
+	
+    def show_difference(self, cube_a, cube_b, entropy=None, accuracy=None):
+        """
+        Show the difference between two data sets.
+        :param cube_a: a cube with the examing data set
+        :param cube_b: a cube with expected results
+        :param accuracy: measurement for each points
+        :param entropy: sum of total delta
+        :return: show difference if fails; otherwise, test passes
+        """
+        result = cube_a.data.flatten()
+        expected = cube_b.data.flatten()
+
+        accuracy = np.repeat(accuracy, len(result))
+
+        if accuracy is None:
+            accuracy = np.repeat(0.001, len(result))
+        if entropy is None:
+            entropy = 0.02
+        # Now assert the data is as expected.
+        delta = np.abs(result - expected)
+        msg = ('\nEntropy: {}\nExpected entropy: {}\n'
+               'Index  -/x   Delta     Target   Expected value   Actual Value'
+               ''.format(delta.sum(), entropy))
+        template = '\n{0:3}     {1:2}  {2:6f}  {3:6f}    {4:6f}      {5:6f}'
+
+        for i, (r_del, t_del, r, t) in enumerate(zip(delta, accuracy,
+                                                     result, expected)):
+            msg += template.format(i, '-' if r_del < t_del else 'x',
+                                   r_del, t_del,
+                                   t, r)
+        # Ensure each accuracy component falls below the target.
+        assert_array_less(delta, accuracy, msg)
+        # Ensure that our entropy is close to the expected one.
+        # If this fails because the result is now smaller than expected, good!
+        # It means we can tighten the expected entropy *AND* the target delta.
+        if np.abs(entropy - delta.sum()) > 0.001:
+            self.fail(msg)
+	
+	
+if __name__ == '__main__':
+    unittest.main()
+
